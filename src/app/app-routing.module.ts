576b393ae108b8ea7e1a7be449dfa39c28e8ed92
vaibhav-gelle <vaibhavgelle@gmail.com>
2022-06-27 16:25:20 +0530

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PredictComponent } from './predict/predict.component';
+import { ResultComponent } from './result/result.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'predict',component:PredictComponent},
+  {path:'result',component:ResultComponent},
+  {path:'home',component:HomeComponent},
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResultComponent } from './result/result.component';
+import { PredictComponent } from './predict/predict.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultComponent,
+    PredictComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
